592da1b8a0e40ab747f3bb7194196292dea69e6f
Ramkumar Ramachandra <artagnon@gmail.com>
2009-06-16 16:36:54 +0530

    Fix parameter lists and remove OAuth code
    Fix parameter lists and remove any OAuth code, since we now use
    oauth.el.

--- twapi.el
+++ twapi.el
@@ -3,7 +3,7 @@
 ;; Search Methods
 (defun twitter-search (query &optional since)
   (twitter-request "GET" (twitter-url (format "%s" "search"))
-		   :parameters '("since" since)))
+		   :parameters '(("since" since))))
 
 (defun twitter-trends ()
   (twitter-request "GET" (twitter-url (format "%s" "trends"))
@@ -11,54 +11,54 @@
 
 (defun twitter-trends-current (since)
   (twitter-request "GET" (twitter-url (format "%s/%s" "statuses" "user_timeline"))
-		   :parameters '("since" since)))
+		   :parameters '(("since" since))))
 
 (defun twitter-trends-daily (since)
   (twitter-request "GET" (twitter-url (format "%s/%s" "statuses" "user_timeline"))
-		   :parameters '("since" since)))
+		   :parameters '(("since" since))))
 
 (defun twitter-trends-weekly (since)
   (twitter-request "GET" (twitter-url (format "%s/%s" "statuses" "user_timeline"))
-		   :parameters '("since" since)))
+		   :parameters '(("since" since))))
 
 ;; Timeline Methods
 (defun twitter-public-timeline (since)
   (twitter-request "GET" (twitter-url (format "%s/%s" "statuses" "public_timeline"))
-		   :parameters '("since" since)))
+		   :parameters '(("since" since))))
 
 (defun twitter-friends-timeline (since)
   (twitter-request "GET" (twitter-url (format "%s/%s" "statuses" "friends_timeline"))
-		   :parameters '("since" since)))
+		   :parameters '(("since" since))))
 
 (defun twitter-user-timeline (&optional since)
   (twitter-request "GET" (twitter-url (format "%s/%s" "statuses" "user_timeline"))
-		   :parameters '("since" since)))
+		   :parameters '(("since" since))))
 
 (defun twitter-mentions (&optional since)
   (twitter-request "GET" (twitter-url (format "%s/%s" "statuses" "mentions"))
-		   :parameters '("since" since)))
+		   :parameters '(("since" since))))
 
 ;; Status Methods
 (defun twitter-show-status (status)
     (twitter-request "GET" (twitter-url (format "%s/%s" "statuses" "show"))
-		     :parameters '("status" status)))
+		     :parameters '(("status" status))))
 
 (defun twitter-update-status (status)
     (twitter-request "GET" (twitter-url (format "%s/%s" "statuses" "update"))
-		     :parameters '("status" status)))
+		     :parameters '(("status" status))))
 
 (defun twitter-destroy-status (status)
     (twitter-request "DELETE" (twitter-url (format "%s/%s" "statuses" "destroy"))
-		     :parameters '("status" status)))
+		     :parameters '(("status" status))))
 
 ;; User Methods
 (defun twitter-show-user (screen-name)
     (twitter-request "GET" (twitter-url (format "%s/%s" "users" "show"))
-		     :parameters '("screen_name" screen-name)))
+		     :parameters '(("screen_name" screen-name))))
 
 (defun twitter-show-friends (search-term)
     (twitter-request "GET" (twitter-url (format "%s/%s" "users" "show"))
-		     :parameters '("search_term" search-term)))
+		     :parameters '(("search_term" search-term))))
 
 ;; Account Methods
 
@@ -70,21 +70,7 @@
 
 ;; Saved Searches Methods
 
-;; OAuth Methods
-(defun twitter-oauth-request-token ()
-    (twitter-request "GET" (twitter-url (format "%s/%s" "oauth" "request_token"))
-		     :parameters '()))
-
-(defun twitter-oauth-authorize ()
-    (twitter-request "GET" (twitter-url (format "%s/%s" "oauth" "authorize"))
-		     :parameters '()))
-
-(defun twitter-oauth-authenticate ()
-    (twitter-request "GET" (twitter-url (format "%s/%s" "oauth" "authenticate"))
-		     :parameters '()))
-
-(defun twitter-oauth-access-token ()
-    (twitter-request "POST" (twitter-url (format "%s/%s" "oauth" "access_token"))
-		     :parameters '()))
+;; OAuth Methods [removed to oauth.el]
+;; OAuth authentication happens in rewrite.el twitel-authenticate()
 
 ;; Help Methods
